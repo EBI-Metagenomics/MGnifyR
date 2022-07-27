faab7c4795c2aa2efc3aa1b76a013d1b221c1fc9
kgatel <kevin.gatel@insa-roeun.fr>
2022-07-27 16:23:08 +0300

    warning message added

--- R/mgnify_get_analyses_treese.R
+++ R/mgnify_get_analyses_treese.R
@@ -33,7 +33,10 @@ mgnify_get_analyses_treese <- function(client = NULL, accessions, usecache=T, re
     #Some biom files don't import - so need a try/catch
     tse_list <- plyr::llply(accessions, function(x) {
         tryCatch(
-            mgnify_get_single_analysis_treese(client, x, usecache = usecache, tax_SU = tax_SU, get_tree = get_tree), error=function(x) NULL)
+            mgnify_get_single_analysis_treese(client, x, usecache = usecache, tax_SU = tax_SU, get_tree = get_tree),
+            warning=function(x){ message("a biom listed in \"accessions\" is missing from the retrieved tse_list")},
+            error=function(x) NULL
+        )
     }, .progress = "text")
 
     #The sample_data has been corrupted by doing the merge (names get messed up and duplicated), so just regrab it with another lapply/rbind
@@ -47,7 +50,7 @@ mgnify_get_analyses_treese <- function(client = NULL, accessions, usecache=T, re
             }
         }
         
-        #Merging every TSE object in the list into a single one***
+        #Merging every TSE object in the list into a single one TSE object
         full_tse <- mia::mergeSEs(tse_list, assay_name = "counts", missing_values = 0)
         full_tse
     }
