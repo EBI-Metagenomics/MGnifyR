1eba46b12ed01a2cae93ca0ac4b903e19403ced6
TuomasBorman <tvborm@utu.fi>
2023-10-30 10:48:54 +0200

    up

--- R/allGenerics.R
+++ R/allGenerics.R
@@ -1,56 +1,70 @@
 # All generic methods are listed here
 
+#' @rdname MgnifyClient-accessors
 #' @export
 setGeneric("url", signature = c("x"), function(x) standardGeneric("url"))
 
+#' @rdname MgnifyClient-accessors
 #' @export
 setGeneric("authTok", signature = c("x"), function(x)
     standardGeneric("authTok"))
 
+#' @rdname MgnifyClient-accessors
 #' @export
 setGeneric("cacheDir", signature = c("x"), function(x)
     standardGeneric("cacheDir"))
 
+#' @rdname MgnifyClient-accessors
 #' @export
 setGeneric("warnings", signature = c("x"), function(x)
     standardGeneric("warnings"))
 
+#' @rdname MgnifyClient-accessors
 #' @export
 setGeneric("useMemCache", signature = c("x"), function(x)
     standardGeneric("useMemCache"))
 
+#' @rdname MgnifyClient-accessors
 #' @export
 setGeneric("memCache", signature = c("x"), function(x)
     standardGeneric("memCache"))
 
+#' @rdname MgnifyClient-accessors
 #' @export
 setGeneric("clearCache", signature = c("x"), function(x)
     standardGeneric("clearCache"))
 
+#' @rdname MgnifyClient-accessors
 #' @export
 setGeneric(
     "url<-", signature = c("x"), function(x, value) standardGeneric("url<-"))
 
+#' @rdname MgnifyClient-accessors
 #' @export
 setGeneric("authTok<-", signature = c("x"), function(x, value)
     standardGeneric("authTok<-"))
 
+#' @rdname MgnifyClient-accessors
 #' @export
 setGeneric("cacheDir<-", signature = c("x"), function(x, value)
     standardGeneric("cacheDir<-"))
 
+#' @rdname MgnifyClient-accessors
 #' @export
 setGeneric("warnings<-", signature = c("x"), function(x, value)
     standardGeneric("warnings<-"))
 
+#' @rdname MgnifyClient-accessors
 #' @export
 setGeneric("useMemCache<-", signature = c("x"), function(x, value)
     standardGeneric("useMemCache<-"))
 
+#' @rdname MgnifyClient-accessors
 #' @export
 setGeneric("memCache<-", signature = c("x"), function(x, value)
     standardGeneric("memCache<-"))
 
+#' @rdname MgnifyClient-accessors
 #' @export
 setGeneric("clearCache<-", signature = c("x"), function(x, value)
     standardGeneric("clearCache<-"))
