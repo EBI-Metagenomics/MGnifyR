4bad8dbf03a767251de9cd1f96f0ac398c771466
TuomasBorman <tvborm@utu.fi>
2024-02-12 15:39:13 +0200

    up

--- R/MGnifyR.R
+++ R/MGnifyR.R
@@ -2,13 +2,13 @@
 #'
 #' \code{MGnifyR} implements an interface to the EBI MGnify database.
 #' See the vignette for a general introduction to this package.
-#' \href{https://www.ebi.ac.uk/metagenomics/about}{About MGnify} for general MGnify
+#' [About MGnify](https://www.ebi.ac.uk/metagenomics/about) for general MGnify
 #' information, and
-#' \href{https://emg-docs.readthedocs.io/en/latest/api.html}{API documentation}
-#' for details about the JSONAPI implementation,
+#' [API documentation](https://emg-docs.readthedocs.io/en/latest/api.html) for
+#' details about the JSONAPI implementation,
 #' @name MGnifyR-package
 #' @aliases MGnifyR
-#' @docType _PACKAGE
+#' @docType package
 #' @seealso \link[TreeSummarizedExperiment:TreeSummarizedExperiment-class]{TreeSummarizedExperiment} class
 NULL
 
