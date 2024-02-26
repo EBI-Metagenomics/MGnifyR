8145fca30bb1f3fd71d455f300f705c26e7e522f
TuomasBorman <tvborm@utu.fi>
2024-02-26 10:30:13 +0200

    Minor typo fix

--- R/MGnifyR.R
+++ R/MGnifyR.R
@@ -5,7 +5,7 @@
 #' [About MGnify](https://www.ebi.ac.uk/metagenomics/about) for general MGnify
 #' information, and
 #' [API documentation](https://emg-docs.readthedocs.io/en/latest/api.html) for
-#' details about the JSONAPI implementation,
+#' details about the JSONAPI implementation.
 #' @name MGnifyR-package
 #' @aliases MGnifyR
 #' @docType package
