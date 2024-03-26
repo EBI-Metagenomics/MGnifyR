044dc3e424c3035e900fdeea86361c7f1e6cbd55
TuomasBorman <tvborm@utu.fi>
2024-03-26 20:20:25 +0200

    Fix links in ?MGnifyR

--- R/MGnifyR.R
+++ R/MGnifyR.R
@@ -2,9 +2,9 @@
 #'
 #' \code{MGnifyR} implements an interface to the EBI MGnify database.
 #' See the vignette for a general introduction to this package.
-#' [About MGnify](https://www.ebi.ac.uk/metagenomics/about) for general MGnify
+#' \href{https://www.ebi.ac.uk/metagenomics/about}{About MGnify} for general MGnify
 #' information, and
-#' [API documentation](https://emg-docs.readthedocs.io/en/latest/api.html) for
+#' \href{https://emg-docs.readthedocs.io/en/latest/api.html}{API documentation} for
 #' details about the JSONAPI implementation.
 #' @name MGnifyR-package
 #' @aliases MGnifyR
